b71c21497c9cfe6f2c4f807eb571a44a38786fc7
Levy Araujo <levy4raujo@gmail.com>
2023-09-15 14:02:28 -0300

    [Requisito 5 concluído.]
    Criando classe Inventory.

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,18 @@
+from typing import Optional
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[list[Product]] = None) -> None:
+        if data is None:
+            self._data: list[Product] = list()
+        else:
+            self._data = data
+
+    def add_data(self, data: list[Product]) -> None:
+        for element in data:
+            self._data.append(element)
+
+    @property
+    def data(self) -> list[Product]:
+        return self._data
