e16e2b5ada4bce7518ad7b07940abb192ba646f7
Levy Araujo <levy4raujo@gmail.com>
2023-09-15 15:35:58 -0300

    [Requisito 6 concluído.]
    Criando Protocolo.

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,2 +1,10 @@
-class Report:
-    pass
+from inventory_report.inventory import Inventory
+from typing import Protocol
+
+
+class Report(Protocol):
+    def add_inventory(self, inventory: Inventory) -> None:
+        """Adicionar no inventario"""
+
+    def generate(self) -> str:
+        """Gerar Report"""
