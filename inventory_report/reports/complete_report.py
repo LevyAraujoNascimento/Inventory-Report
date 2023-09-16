c2441652d0c548aabaf1bf8cb93c7e2c7c83e733
Levy Araujo <levy4raujo@gmail.com>
2023-09-16 13:06:23 -0300

    [Requisito 9 em andamento.]
    Rel. Completo.

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,27 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    def companies_stock(self) -> dict[str, int]:
+        companies: dict[str, int] = {}
+
+        for invent in self._invetories:
+            for product in invent._data:
+                comp_name = product.company_name
+                if comp_name in companies:
+                    companies[comp_name] += 1
+                else:
+                    companies[comp_name] = 1
+
+        return companies
+
+    def generate(self) -> str:
+        companies = self.companies_stock()
+        report = super().generate()
+
+        final_report = f"{report}\nStocked products by company:\n"
+
+        for company, quant in companies.items():
+            final_report += f"- {company}: {quant}\n"
+
+        return final_report
