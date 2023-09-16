3f6123661e707c2ea526c528cc64297bb21b84cd
Levy Araujo <levy4raujo@gmail.com>
2023-09-16 13:36:02 -0300

    [Requisito 9 concluído.]
    Rel. Completo concluído.

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,3 +1,4 @@
+from datetime import datetime, date
 from inventory_report.reports.simple_report import SimpleReport
 
 
@@ -16,12 +17,38 @@ class CompleteReport(SimpleReport):
         return companies
 
     def generate(self) -> str:
+        oldestDate = None
+        closestDate = None
+        largInvent = self.largest_inventory()
+
+        today = date.today()
+
+        for invent in self._invetories:
+            for product in invent._data:
+                manu = product.manufacturing_date
+                exp = product.expiration_date
+                dateManu = datetime.strptime(manu, '%Y-%m-%d').date()
+                dateExp = datetime.strptime(exp, '%Y-%m-%d').date()
+
+                if oldestDate is None or dateManu < oldestDate:
+                    oldestDate = dateManu
+
+                if dateExp >= today and (
+                    closestDate is None
+                    or dateExp < closestDate
+                ):
+                    closestDate = dateExp
+
+        result = (
+            f"Oldest manufacturing date: {oldestDate}\n"
+            f"Closest expiration date: {closestDate}\n"
+            f"Company with the largest inventory: {largInvent}\n"
+            f"Stocked products by company:\n"
+        )
+
         companies = self.companies_stock()
-        report = super().generate()
 
-        final_report = f"{report}\nStocked products by company:\n"
+        for name, count in companies.items():
+            result += f"- {name}: {count}\n"
 
-        for company, quant in companies.items():
-            final_report += f"- {company}: {quant}\n"
-
-        return final_report
+        return result
