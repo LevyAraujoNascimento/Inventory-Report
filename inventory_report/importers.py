807dd03e3dae819fe0aeb9748fe788be5b3c8808
Levy Araujo <levy4raujo@gmail.com>
2023-09-16 12:04:01 -0300

    [Requisito 8 concluído.]
    Teste Csv Import.

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -2,6 +2,7 @@ from inventory_report.product import Product
 from abc import ABC, abstractmethod
 from typing import Dict, Type
 import json
+import csv
 
 
 class Importer(ABC):
@@ -26,8 +27,26 @@ class JsonImporter(Importer):
         return result
 
 
-class CsvImporter:
-    pass
+class CsvImporter(Importer):
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    def import_data(self) -> list[Product]:
+        result = list()
+        with open(self.path, encoding="utf-8") as file:
+            reader = csv.DictReader(file, delimiter=",", quotechar='"')
+            for row in reader:
+                result.append(Product(
+                    id=row["id"],
+                    product_name=row["product_name"],
+                    company_name=row["company_name"],
+                    manufacturing_date=row["manufacturing_date"],
+                    expiration_date=row["expiration_date"],
+                    serial_number=row["serial_number"],
+                    storage_instructions=row["storage_instructions"],
+                ))
+
+        return result
 
 
 # Não altere a variável abaixo
